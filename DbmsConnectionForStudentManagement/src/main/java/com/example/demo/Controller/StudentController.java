9a3cf78569b34e76d5f6947e07ff839634668e59
pk <pk@192.168.100.7>
2019-01-27 19:19:19 +0530

    After creating User Interface with All requests(GET,POST,PUT,DELETE)

--- DbmsConnectionForStudentManagement/src/main/java/com/example/demo/Controller/StudentController.java
+++ DbmsConnectionForStudentManagement/src/main/java/com/example/demo/Controller/StudentController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -27,21 +28,23 @@ public class StudentController {
 	@Autowired
 	private StudentRepo str;
 	
-	@GetMapping("/getAllStudents")
+	@GetMapping(value="/getAllStudents",produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<Student> gettingStudent(Student p1) {
-		System.out.println(str.findAll());
+	//	System.out.println(str.findAll());
 		return str.findAll();
 	}
 	
-	@RequestMapping(value="/getStudentById/{Id}")
-	public Optional<Student> GetStudById(@PathVariable int Id,Model m) {
-		Optional<Student> std=str.findById(Id);
+	@GetMapping(value="/getStudentById/{Id}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public Student GetStudById(@PathVariable int Id,Model m) {
+		Student std=str.findById(Id).orElse(new Student());
+		//System.out.println("getStudentbyid is called"+Id);
+		//System.out.println(std.getName());
 		return std;
 	}
 	
 	@PostMapping("/AddStudent")
 	public Student AddingStudent(@RequestBody Student p1) {
-		System.out.println(p1.getId());
+		//System.out.println(p1.getId());
 		str.save(p1);
 		return p1;
 	}
@@ -52,7 +55,7 @@ public class StudentController {
 		str.delete(st);
 		
 	}
-	@PutMapping("/UpdateStudent")
+	@PutMapping("/UpdateStudent/{id}")
 	public Student UpdatingStudent(@RequestBody Student p1) {
 		str.save(p1);
 		return p1;
